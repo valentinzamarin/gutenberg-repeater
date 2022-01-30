e629176b0046a81b9b5c9f8bdadd36067e51945a
valentinzm <zamarin.dev@gmail.com>
2022-01-30 22:51:27 +0300

    Simple todo list

--- src/block/block.js
+++ src/block/block.js
@@ -1,98 +1,92 @@
-/**
- * BLOCK: gutenberg-repeater
- *
- * Registering a basic block with Gutenberg.
- * Simple block, renders and saves the same content without any interactivity.
- */
-
-//  Import CSS.
 import './editor.scss';
 import './style.scss';
 
-const { __ } = wp.i18n; // Import __() from wp.i18n
-const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
+const { __ } = wp.i18n;
+const { registerBlockType } = wp.blocks;
+const {
+	Button,
+	TextControl,
+	IconButton,
+} = wp.components;
 
-/**
- * Register: aa Gutenberg Block.
- *
- * Registers a new block provided a unique name and an object defining its
- * behavior. Once registered, the block is made editor as an option to any
- * editor interface where blocks are implemented.
- *
- * @link https://wordpress.org/gutenberg/handbook/block-api/
- * @param  {string}   name     Block name.
- * @param  {Object}   settings Block settings.
- * @return {?WPBlock}          The block, if it has been successfully
- *                             registered; otherwise `undefined`.
- */
+const {
+	Fragment,
+} = wp.element;
+
 registerBlockType( 'cgb/block-gutenberg-repeater', {
-	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
-	title: __( 'gutenberg-repeater - CGB Block' ), // Block title.
-	icon: 'shield', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
-	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+	title: __( 'gutenberg-repeater' ),
+	icon: 'list-view',
+	category: 'common',
 	keywords: [
-		__( 'gutenberg-repeater — CGB Block' ),
-		__( 'CGB Example' ),
-		__( 'create-guten-block' ),
+		__( 'gutenberg-repeater' ),
 	],
+	attributes: {
+		repeater: {
+			type: 'array',
+			default: [],
+		},
+	},
 
-	/**
-	 * The edit function describes the structure of your block in the context of the editor.
-	 * This represents what the editor will render when the block is used.
-	 *
-	 * The "edit" property must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 *
-	 * @param {Object} props Props.
-	 * @returns {Mixed} JSX Component.
-	 */
 	edit: ( props ) => {
-		// Creates a <p class='wp-block-cgb-block-gutenberg-repeater'></p>.
+		const handleAddField  = () => {
+			const repeater = [ ...props.attributes.repeater ];
+			repeater.push( {
+				item: '',
+			} );
+			props.setAttributes( { repeater } );
+		}
+
+		const handleChangeField = ( item, idx ) => {
+			const repeater = [ ...props.attributes.repeater ];
+			repeater[ idx ].item = item;
+			props.setAttributes( { repeater } );
+		}
+
+		const handleRemoveItem = ( idx ) => {
+			const repeater = [ ...props.attributes.repeater ];
+			repeater.splice( idx, 1 );
+			props.setAttributes( { repeater } );
+		}
+
+		let repeaterField;
+		if( props.attributes.repeater.length ) {
+			repeaterField = props.attributes.repeater.map( ( repeater, idx ) => {
+				return <Fragment key={ idx }>
+					<div className="repeater-item">
+						<TextControl
+							placeholder={ __( 'Repeater item' ) }
+							value={ props.attributes.repeater[ idx ].item }
+							onChange={ ( item ) => handleChangeField( item, idx ) }
+						/>
+						<IconButton
+							icon="no-alt"
+							label={ __( 'Remove item' ) }
+							onClick={ () => handleRemoveItem( idx ) }
+						/>
+					</div>
+				</Fragment>;
+			})
+		}
 		return (
 			<div className={ props.className }>
-				<p>— Hello from the backend.</p>
-				<p>
-					CGB BLOCK: <code>gutenberg-repeater</code> is a new Gutenberg block
-				</p>
-				<p>
-					It was created via{ ' ' }
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>.
-				</p>
+				{repeaterField}
+				<Button
+					onClick={ () => handleAddField() }
+					className="button button-primary"
+				>
+					{ __( 'Add Field' ) }
+				</Button>
 			</div>
 		);
 	},
 
-	/**
-	 * The save function defines the way in which the different attributes should be combined
-	 * into the final markup, which is then serialized by Gutenberg into post_content.
-	 *
-	 * The "save" property must be specified and must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 *
-	 * @param {Object} props Props.
-	 * @returns {Mixed} JSX Frontend HTML.
-	 */
 	save: ( props ) => {
+		const repeaterFront = props.attributes.repeater.map( ( repeater, idx ) => {
+			return <p key={ idx }>{ repeater.item }</p>;
+		});
 		return (
 			<div className={ props.className }>
-				<p>— Hello from the frontend.</p>
-				<p>
-					CGB BLOCK: <code>gutenberg-repeater</code> is a new Gutenberg block.
-				</p>
-				<p>
-					It was created via{ ' ' }
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>.
-				</p>
+				{repeaterFront}
 			</div>
 		);
 	},
